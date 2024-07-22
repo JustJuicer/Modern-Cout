d84ba347ca69d9cee86cf402b609658f4aeb7b99
jujinqian162 <ju2237210046@gmail.com>
2024-07-22 12:34:36 +0800

    add cplusplus

--- test/print/main.cpp
+++ test/print/main.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 using namespace ju;
 int main(){
-    coutln("printing test");
+
+    coutln("printing test: __cplusplus: ", __cplusplus);
     cred("red\n");
     cgreen("green\n");
     cblue("blue\n");
     cout("to cover");
     rout("cover\n");
-    coutln("vector: ", std::vector{1, 2, 3, 4, 5});
+    // coutln("vector: ", std::vector{1, 2, 3, 4, 5});
 }
