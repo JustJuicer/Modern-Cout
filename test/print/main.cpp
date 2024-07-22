7ba2940adf43f128265741fd462de47bee0611e1
jujinqian162 <ju2237210046@gmail.com>
2024-07-22 13:15:48 +0800

    final

--- test/print/main.cpp
+++ test/print/main.cpp
@@ -1,5 +1,4 @@
 #include "cout.hpp"
-#include <ranges>
 #include <vector>
 using namespace ju;
 int main(){
@@ -10,7 +9,5 @@ int main(){
     cblue("blue\n");
     cout("to cover");
     rout("cover\n");
-    static_assert(!string_able<std::vector<int>>);
-    static_assert(std::ranges::viewable_range<std::vector<int>>);
     coutln("vector: ", std::vector{1, 2, 3, 4, 5});
 }
